8f2297cda8c995f253e94f7cdbbdc849dd37cacd
Shuu2806 <odanlafrance28@gmail.com>
2023-12-15 14:54:36 +0100

    renplancement

--- StudentCode.py
+++ StudentCode.py
@@ -65,11 +65,11 @@ def instance_selection(instance_num):
         return "Wash","ghz",27    
     elif instance_num==9:
         return "Cairo","qaoa",14
-    elif instance_num==11:
+    elif instance_num==10:
         return "Singapore","ghzall",19
-    elif instance_num==12:
+    elif instance_num==11:
         return "Singapore","dj",19
-    elif instance_num==13:
+    elif instance_num==12:
         return "Cairo","ghz",19
     else:
         print("Choix d'une instance inexistance, instance 1 revoyé  par défaut")
@@ -80,7 +80,8 @@ def instance_selection(instance_num):
 ##     Pour choisir une instance: 
 ##     Modifier instance_num ET RIEN D'AUTRE    
 ##-------------------------------------------------------
-instance_num=2     #### Entre 1 et 9 inclue
+print("\n Choisi l'instance (Entre 1 et 12 inclue) : ",end="")
+instance_num=int(input()) # permet de choisir l'instance au début du programme
 
 backend_name,circuit_type,num_qubit=instance_selection(instance_num)
 backend,qc,qr=instance_characteristic(backend_name,circuit_type,num_qubit)
@@ -89,255 +90,144 @@ n=num_qubit
 m=backend.num_qubits
 
 ##-------------------------------------------------------
-##     A vous de jouer !  
+##  Class Solution
 ##-------------------------------------------------------
 
-# Initialisation de la meilleure solution de l'exécution
-def set_best_init(best):
-    global best_solution_instance
-    if(best_solution_instance == None or best_solution_instance.cost > best.cost):
-        best_solution_instance = best
-
-##-------------------------------------------------------
-##     Classes
-##-------------------------------------------------------
-        
-# Classe d'une solution et de son coût
 class Solution:
     solution = None
     cost = None
 
-    def __init__(self,solution=None,cost=None):
-        if(solution == None):
-            self.solution = random.sample(range(m),n)           # Génération aléatoire d'une solution valide (pour diversification)
-        else:
-            self.solution = solution
+    def __init__(self, solution, cost=None):
+        self.solution = solution if isinstance(solution,np.ndarray) else np.array(solution) # enregistre la solution donnée, transforme en np.array si pas déjà fait
+        self.cost = cost if cost is not None else fitness(self.solution) # calcule le fitness si le cost n'est pas donnée
 
-        if(cost == None):
-            self.cost = fitness(self.solution)                  # Calcul du coût de la solution
-        else:
-            self.cost = cost
-
-    # Clonage d'une solution et son coût
-    def clone(self):
-        return Solution(solution=self.solution.copy(),cost=self.cost)
-
-# Classe d'une solution résultante d'une reproduction
-class ChildSolution(Solution):
-    father_solution = None
-    mother_solution = None
-
-    def __init__(self,father_solution,mother_solution):
-        self.father_solution = father_solution.clone()
-        self.mother_solution = mother_solution.clone()
-
-        super().__init__(solution=self.cross_two_points())      # Obtention de la solution enfant par croisement à deux points
-
-    # Croisement à deux points aléatoires
-    def cross_two_points(self):
-        first_point = random.randint(0,n-1)
-        second_point = random.randint(first_point,n-1)
-
-        first_part = (self.father_solution.solution.copy())[0:first_point]
-        third_part = (self.father_solution.solution.copy())[second_point:n]
-
-        list_tmp_first = [x for x in self.mother_solution.solution if x not in first_part]
-        list_tmp_second = [x for x in list_tmp_first if x not in third_part]
-
-        second_part = list_tmp_second[0 : n - len(first_part) - len(third_part)]
-
-        return (first_part + second_part + third_part)
-    
-    # Mutation aléatoire d'un élément
-    def mutation(self):
+    def mutation(self): # change un chiffre par un chiffre non-présent dans la liste d'interger
         list_possibilities = [x for x in range(m) if x not in self.solution]
         if(list_possibilities != []):
             mutation_value = list_possibilities[random.randint(0,len(list_possibilities)-1)]
-            self.solution[random.randint(0,len(self.solution)-1)] = mutation_value
+            self.solution[random.randint(0,n-1)] = mutation_value
 
-# Classe d'une population (ensemble de solutions) avec sa meilleure solution
-class Population:
-    best_solution = None
-    pop = list()
-
-    def __init__(self,size):
-        self.pop.clear()
-        print("Initialisation de la population :")
-        self.initialize_solutions(size)
-
-    def show_best_solution(self):
-        print("Best Solution : \n")
-        print(f"Solution : {self.best_solution.solution} with cost : {self.best_solution.cost} \n")
-
-    def show_actual_population(self):
-        print(f"nombre d'individu : {len(self.pop)}")
-        for sol in self.pop: 
-            print(f"Solution : {sol.solution} with cost : {sol.cost}")
-        print(f"\n")
-
-    # Vérification de meilleure solution ou non
-    def is_best_solution(self,new_solution):
-        if(self.best_solution == None or new_solution.cost < self.best_solution.cost):
-            self.best_solution = new_solution
-            set_best_init(self.best_solution)
-
-    # Initialisation de la population par ajout de solutions aléatoires
-    def initialize_solutions(self,size):
-        for i in range(size):
-            print(f"{round((100/size)*i,2)}%",end="\r")
-            self.add_solution(Solution())
-
-    def add_solution(self,new_solution):
-        self.pop.append(new_solution)
-        self.is_best_solution(new_solution)             # Vérification de meilleure solution
-
-    # Choisi un nombre entré de solutions en fonction de probabilités inverse aux coûts
-    def choose_solutions(self,size):
-        inverted_costs = [1 / s.cost for s in self.pop]
-        total_inverted_cost = sum(inverted_costs)
-        probability_cost = [inverted_cost / total_inverted_cost for inverted_cost in inverted_costs]
-        return list(np.random.choice(self.pop,size,False,probability_cost))                             # Liste de solutions de la population
+    def best_additive_neighbor(self): # Choisi la meilleur solution dans un voisinage [] + i (ex m=20 n=4 => 1 4 5 20 => 2 5 6 0 => 3 6 7 1 => ...)
+        npneighbors = np.array([Solution((self.solution + (i+1)) % m) for i in range(m-1)])
+        npneighbors = np.append(npneighbors,self)
+        return min(npneighbors, key=lambda x: x.cost)
     
-    # Effectue un nombre entré de reproduction entre deux parents
-    def reproduce(self,number_of_childs):
-        childs = list()
+    def best_insertion_neighbor(self):
+        npneighbors = np.array(self)
+        for i in range(n-2):
+            current_solution = self.solution.copy()
+            npneighbors = np.append(npneighbors,Solution(np.concatenate([current_solution[(i+1):], current_solution[:(i+1)]])))
 
-        for i in range(number_of_childs):
-            couple = self.choose_solutions(2)                           # Choisi un couple sur base de probabilités
+        return min(npneighbors, key=lambda x: x.cost)
+    
+    def best_inversion_neighbor(self):
+        npneighbors = np.array(self)
+        for i in range(n-1):
+            current_solution = self.solution.copy()
+            current_solution[i],current_solution[i+1] = current_solution[i+1],current_solution[i]
+            npneighbors = np.append(npneighbors,Solution(current_solution))
 
-            child = ChildSolution(couple[0],couple[1])                  # Créé un enfant croisé
+        return min(npneighbors, key=lambda x: x.cost)
+            
+##-------------------------------------------------------
+##  Class Population
+##-------------------------------------------------------
 
-            # Probabilité que l'enfant mute
+class Population:
+    history_population = None
+    current_population = None
+    current_population_size = None
+    
+    def __init__(self, population_size): # enregistre la taille et créer une nouvelle population random de taille choisi
+        print(f"\nInit de la population...", end="\n")
+        self.current_population_size = population_size
+        self.current_population = np.array([Solution(np.random.permutation(m)[:n]) for _ in range(population_size)], dtype=object)
+        self.history_population = self.current_population.copy()
+        print(f"Population Init", end="\n\n")
+        for sol in self.current_population:
+            print(f"Initial Population | solution : {sol.solution} cost : {sol.cost}", end="\n")
+
+    def reproduce(self,b_time,time_in_secondes):
+        print("\nReproduce...")
+        for _ in range(size_population // 4 ): # fait 1/4 de la taille de la population de nouveau enfant
+            print("Creation d'un enfant", end="\r")
+            child = self.cross_two_points(parent=self.choose_solutions(self.current_population_size)) # prend 2 parents pour faire un enfant avec la technique des 2 point
+            
             if(random.randint(0,99) < mutation_probability):
                 child.mutation()
+            
+            while(time.time() - b_time < time_in_secondes): # boucle pour avoir le meilleur min entre les voisins
+                          
+                print("Recherche locale de l'enfant", end="\r")
+                best = child.best_insertion_neighbor()
+                best = child.best_inversion_neighbor()
+                
+                self.history_population = np.append(self.history_population, best) # ajoute le best local dans l'historique
+                
+                if(best.cost == child.cost): # si le current et le même que le best alors c'est un min local donc sort de la boucle
+                    print(f"Minimum locale | solution : {best.solution} cost : {best.cost}", end="\n")
+                    self.current_population = np.append(self.current_population, best)
+                    break
+                else: # sinon recommence avec le nouveau minimum trouvé
+                    child = best 
+                
+            if(time.time() - b_time >= time_in_secondes):   # si le temps est dépasser sors du for
+                    break 
+                  
+        self.current_population = self.choose_solutions(self.current_population_size) # Ajuste la taille de la pop pour reter à sa taille initiale
+            
+    def cross_two_points(self,parent):
+        first_point = np.random.randint(0, n-1) # choisi le 1er point de coupe
+        second_point = np.random.randint(first_point, n-1) # choisi le 2e point de coupe
+        who = np.random.randint(0, 1) # choisi qui va être le père et la mère
 
-            # Effectue une recherche locale pour intensifier l'enfant trouvé
-            print(f"{i+1}e child :")
-            child = best_local_children(child)
+        first_part = parent[who].solution[:first_point] # prend la 1er et 3e partie au père
+        third_part = parent[who].solution[second_point:n]
 
-            childs.append(child)
+        second_part = np.setdiff1d(parent[1-who].solution, np.concatenate([first_part, third_part]))[:n - len(first_part) - len(third_part)]
+        # prend la 2e partie en retirant les doublons de la 1er et 3e partie et coupe tout le trop
+        
+        return Solution(np.concatenate([first_part, second_part, third_part])) # retourne une nouvelle sol avec les infos des parties
 
-        # Ajoute les enfants à la population
-        for c in childs:
-            self.add_solution(c)
+    def choose_solutions(self,size): # permet de chosir un nombre de membre dans la population actuelle (plus le coût est petit, plus il a de chance)
+        inverted_costs = [1 / s.cost for s in self.current_population]
+        total_inverted_cost = sum(inverted_costs)
+        probability_cost = [inverted_cost / total_inverted_cost for inverted_cost in inverted_costs]
+        return np.random.choice(self.current_population,size,False,probability_cost)
 
-        # Régule la population par probabilités pour ne pas déppasser la taille fixée
-        self.pop = self.choose_solutions(size_population)
+    def show_current_best_solution(self): # donne la meilleur solution de toute la population
+        return min(self.history_population, key=lambda x: x.cost)
 
 ##-------------------------------------------------------
-##     Recherches locales
+##  Function thread show max
 ##-------------------------------------------------------
-        
-# Recherche locale par permutation
-def best_permutation_neighbor(current):
-    best = current.clone()
-    for i in range(n):
-        print(f"{round((100/(n))*i,2)}%",end="\r")
-        for j in range(n):
-            neighbor_sol = current.solution.copy()
-            
-            neighbor_sol[i],neighbor_sol[j] = neighbor_sol[j],neighbor_sol[i]               # Échanges entre chaque élément de la solution
-            neighbor = Solution(solution=neighbor_sol)
-            
-            if(neighbor.cost < best.cost):
-                best = neighbor.clone()
-    return best
 
-# Recherche locale par inversion
-def best_inversion_neighbor(current):
-    best = current.clone()
-    for i in range(n-1):
-        print(f"{round((50/(n-1))*i,2)}%",end="\r")
-        neighbor_sol = current.solution.copy()
-
-        neighbor_sol[i],neighbor_sol[i+1] = neighbor_sol[i+1],neighbor_sol[i]               # Échanges entre chaque élément consécutif de la solution
-        neighbor = Solution(solution=neighbor_sol)
-        
-        if(neighbor.cost < best.cost):
-            best = neighbor.clone()
-    
-    return best
-
-# Recherche locale par insertion
-def best_insertion_neighbor(current):
-    best = current.clone()
-    for i in range(n-1):
-        print(f"{round(50+(50/(n-1))*i,2)}%",end="\r")
-        neighbor_sol = current.solution.copy()
-
-        insert = neighbor_sol.pop(i)                            # Déplacement de chaque élément à la fin de la solution (décalage de la solution)
-        neighbor_sol.append(insert)
-        neighbor = Solution(solution=neighbor_sol)
-
-        if(neighbor.cost < best.cost):
-            best = neighbor
-
-    return best
-
-# Recherche locale générale
-def best_local_children(child):
-    best = child.clone()
-    current = child.clone()
-    
-    # Boucle infinie jusqu'à trouver l'optimum local
-    while(True):
-        b_time = time.time()                                                                # Double recherche locale chronométrée (inversion -> insertion)
-        current = best_inversion_neighbor(current)                                          
-        current = best_insertion_neighbor(current)
-        print(f"Cette bonne méthode a pris : {round(time.time() - b_time,2)}s")
-
-        if(best.cost > current.cost):                                                   # Vérification d'amélioration locale : Oui -> on continu
-            best = current.clone()                                                      #                                      Non -> break (optimum local trouvé)
-            set_best_init(best)
-            print(f">>current change : {best.solution} cost : {best.cost}")
-        else:
-            print(f">local best : {best.solution} cost : {best.cost}",end="\n\n")
-            break
-            
-    return best
-
-# Donne l'information sur la meilleure solution de l'exécution à tout moment
 def show_best_now():
-    global best_solution_instance
+    global population
     b_time = time.time()
     while(True):
         input()
-        print(f"Running time : {round(time.time() - b_time,2)}s")
-        if(best_solution_instance != None):
-            print(f"The best solution is {best_solution_instance.solution} with a cost {best_solution_instance.cost} \n")
-        else: 
-            print("N'a pas encore trouvé de meilleur solution")
-           
-##-------------------------------------------------------
-##     Constantes du programme
-##-------------------------------------------------------
-            
-best_solution_instance = None
- 
-t = threading.Thread(target=show_best_now)
-t.start()
+        print(f"running time : {round(time.time() - b_time,2)}s")
+        current_best_solution = population.show_current_best_solution()
+        print(f"Meilleur solution : {current_best_solution.solution} cost {current_best_solution.cost}")
 
-# Taille de population en fonction des variables m et n de l'instance (m et n + grand -> population + petite)
-size_population = 10
-size_population += int(size_population * (size_population/(m+n)))
+##-------------------------------------------------------
+##  Main
+##-------------------------------------------------------
 
-half_size_population=size_population//2
-number_of_reproduction_per_population = 10
+size_population = 25
 mutation_probability = 25
+time_in_secondes = 5*60
+b_time = time.time()
 
-number_of_population_generate = 0
+population = Population(size_population) # Création de la population
 
-##-------------------------------------------------------
-##     Boucle générale du programme
-##-------------------------------------------------------
+t = threading.Thread(target=show_best_now,daemon=True)
+t.start() # lancement du thread pour voir le meilleur
 
-# Boucle infinie du programme
-while True: 
-    population = Population(size_population)                        # Génération d'une nouvelle population avec affichage
-    number_of_population_generate += 1
-    print(f"Population pool {number_of_population_generate}\n")
-    population.show_actual_population()
-
-    for i in range(number_of_reproduction_per_population):          # Reproduction de la population courante (sélection, croisement, mutation, recherche locale, régulation)
-        print(f"{i+1}e reproducing... \n")
-        population.reproduce(half_size_population)
+while(time.time() - b_time < time_in_secondes): # fin de la boucle après Xsecondes
+    population.reproduce(b_time,time_in_secondes) # lance une reporduction
+    
+print("=========================\n Temps alloué fini\n========================= ")
+current_best_solution = population.show_current_best_solution() #fin du programme et affiche la meilleur sol
+print(f"Meilleur solution : {current_best_solution.solution} cost {current_best_solution.cost}") 
